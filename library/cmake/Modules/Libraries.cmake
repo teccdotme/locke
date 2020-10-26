4beb30f3e7c16c20d0ee48e66ed2f4e1232aa231
tecc <tecc@tecc.me>
2020-10-26 22:43:52 +0100

    added llvm as a library
    sorry if it causes problems, did for me but i fixed it easily

--- library/cmake/Modules/Libraries.cmake
+++ library/cmake/Modules/Libraries.cmake
@@ -2,6 +2,10 @@ message(STATUS "Finding libraries..")
 
 find_package(PCRE2  REQUIRED)
 find_package(JPCRE2 REQUIRED)
+find_package(LLVM   CONFIG REQUIRED)
 
-set(LIBRARIES ${PCRE2_LIBRARIES})
-set(INCLUDE_DIRS ${PCRE2_INCLUDE_DIRS} ${JPCRE2_INCLUDE_DIRS})
+llvm_map_components_to_libnames(LLVM_LIBRARIES support core irreader)
+add_definitions(${LLVM_DEFINITIONS})
+
+set(LIBRARIES ${PCRE2_LIBRARIES} ${LLVM_LIBRARIES})
+set(INCLUDE_DIRS ${PCRE2_INCLUDE_DIRS} ${JPCRE2_INCLUDE_DIRS} ${LLVM_INCLUDE_DIRS})
